52aec1d42e38f48015d30afcde9307aafd0b5673
blackSkr <briliansatria2@gmail.com>
2025-05-28 09:27:59 +0800

    Fix tailwind

--- resources/views/landingpage/index.blade.php
+++ resources/views/landingpage/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservasi Gedung | Pams</title>
+        @vite('resources/css/app.css')
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="icon" type="image/x-icon" href="logo-web-2.svg">
